7954eb21c57ba6e152911c06d384e63a67e03eab
ES-KepKep <eskepkep@gmail.com>
2022-10-22 10:36:37 +0300

    dialouger alternative
    made an alternative with dialoguer for practice

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use dialoguer::Input;
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
 
 fn main() {
     println!("Guess The number");
@@ -8,16 +8,14 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        
+        let guess = Input::<String>::new()
+            .with_prompt("Your guess")
+            .interact_text()
+            .unwrap();
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(x) => x,
+            Ok(i) => i,
             Err(_) => {
                 println!("Not a number!\n");
                 continue;
